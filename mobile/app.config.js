ad368722375555319bc4cac2d6ff1f4243c165a6
kopansE <evyatar.kopans@gmail.com>
2026-03-09 16:10:20 +0200

    updated version number

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "כחולבן",
     slug: "kahol-lavan",
-    version: "1.0.0",
+    version: "1.1.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
